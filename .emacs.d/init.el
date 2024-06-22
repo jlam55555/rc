4b8d9e4531cba4f6b8561c217edd807bcaa18f32
Jonathan Lam <jlam55555@gmail.com>
2024-06-22 01:34:58 -0400

    Add lsp-ui config
    This package is actually pretty amazing. I shied away from this at first because
    I was worried about performance but it really doesn't seem like an issue, at
    least for personal projects.
    
    counsel-rg is pretty cool as well. Definitely will replace my usages of
    projectile-ripgrep.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,6 +45,7 @@
   (eval-expression-minibuffer-setup-hook . enable-paredit-mode))
 
 ;; windmove.el
+;; C-S-* works on my work computer but doesn't seem to work here.
 (use-package windmove
   :bind (("M-<left>" . windmove-left)
 	 ("M-<right>" . windmove-right)
@@ -65,11 +66,13 @@
 ;; ivy/counsel/swiper: narrowing completion tools
 (use-package ivy
   :diminish
+  :bind ("C-c C-z" . ivy-toggle-fuzzy)
   :config
   (setq ivy-re-builders-alist
 	;; Fuzzy search for commands only (M-x). This would
 	;; give too many matches for swiper.
-	'((counsel-M-x . ivy--regex-fuzzy)
+	'((ivy-completion-in-region . ivy--regex-fuzzy)
+	  (counsel-M-x . ivy--regex-fuzzy)
 	  (t . ivy--regex-plus)))
   (ivy-mode))
 (use-package swiper
@@ -80,11 +83,7 @@
   :diminish
   :after ivy
   :config (counsel-mode)
-  ;; Finish company completions in minibuffer.
-  ;; Alternatively (and probably better) is complete-symbol,
-  ;; which allows you to do a narrowing search inline.
-  ;; Which maybe makes company-mode obsolete.
-  :bind ("C-c C-x C-c" . counsel-company))
+  :bind ("C-c C-r" . counsel-rg))
 
 ;; flycheck.el: syntax checking (& integration with lsp)
 (use-package flycheck
@@ -97,16 +96,30 @@
   :config
   ;; If lsp doesn't exist or isn't installed, don't warn.
   (setq lsp-warn-no-matched-clients nil))
-(use-package lsp-ivy
-  :after lsp-mode
-  :commands lsp-ivy-workspace-symbol)
+(use-package lsp-ui
+  :config
+  ;; lsp-ui-sideline
+  (setq lsp-ui-sideline-show-diagnostics t)
+  ;; lsp-ui-peek
+  (setq lsp-ui-peek-always-show t)
+  (define-key lsp-ui-mode-map
+	      [remap xref-find-definitions]
+	      'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map
+	      [remap xref-find-references]
+	      'lsp-ui-peek-find-references)
+  ;; lsp-ui-doc
+  (setq lsp-ui-doc-show-with-cursor t)
+  (setq lsp-ui-doc-enhanced-markdown t)
+  ;; This only seems to work in graphical mode.
+  ;; Doc comments also only seem to show in graphical mode.
+  (setq lsp-ui-doc-position 'at-point))
 
 ;; projectile.el: package management
 (use-package projectile
   :init (projectile-mode +1)
   :bind (:map projectile-mode-map
 	      ("C-c p" . projectile-command-map)))
-(use-package projectile-ripgrep)
 
 ;; company.el: completion front-end
 (use-package company
@@ -122,7 +135,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(leuven))
  '(package-selected-packages
-   '(company projectile-ripgrep projectile lsp-ivy lsp-mode flycheck diminish counsel swiper ivy format-all paredit expand-region)))
+   '(lsp-ui company projectile lsp-mode flycheck diminish counsel swiper ivy format-all paredit expand-region)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
